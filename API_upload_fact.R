51cfad27f4e8de4f75d7fa5673127dc5756f05eb
Triumrus <kam20011989@gmail.com>
2017-11-14 16:51:01 +0300

    Update API_upload_fact.R

--- API_upload_fact.R
+++ API_upload_fact.R
@@ -2,14 +2,14 @@ library(RCurl)
 library(jsonlite)
 library(data.table)
 library(RJDBC)
-train<- fread("ftp://api:vfnhbo8934ykfjhg@185.220.32.98/clients/energoeffect/upload/fact.csv")
+train<- fread("ftp://login:password@185.220.32.98/clients/energoeffect/upload/fact.csv")
 
 
 
 drv <- JDBC("com.mysql.jdbc.Driver",
             "C:/Users/user/Documents/Downloads/sqldeveloper/jdbc/lib/mysql-connector-java-3.1.14/mysql-connector-java-3.1.14-bin.jar", "`")
 
-con <- dbConnect(drv,"jdbc:mysql://185.220.32.98:3306/energo","api", "vfnhbo8934ykfjhg")
+con <- dbConnect(drv,"jdbc:mysql://185.220.32.98:3306/energo","login", "password")
 train$DATE_ZALIVA <- Sys.Date()
 train$DATE <- as.character(train$DATE)
 train$DATE_ZALIVA <- as.character(train$DATE_ZALIVA)
@@ -22,5 +22,5 @@ for(i in 1:nrow(train)){
   print(i/nrow(train))
 }
 
-# TODO сделать удаление "ftp://api:vfnhbo8934ykfjhg@185.220.32.98/clients/energoeffect/upload/fact.csv"
+# TODO сделать удаление "ftp://login:password@185.220.32.98/clients/energoeffect/upload/fact.csv"
 dbDisconnect(con)
