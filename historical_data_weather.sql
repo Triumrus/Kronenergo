f95dc112b6444a03adf6ec7cb9fa477631a55b22
Triumrus <kam20011989@gmail.com>
2017-11-23 17:48:16 +0300

    Create historical_data_weather.sql

---
+++ historical_data_weather.sql
@@ -0,0 +1,44 @@
+#Преобразование столбцов из текста в DECIMAL . Для данных из History Bulk
+SET SQL_SAFE_UPDATES = 0;
+SET SESSION sql_mode = '';
+update energo.historical_data_weather set
+snow_1h = case
+ when CONVERT(snow_1h,DECIMAL(9,3)) =0 then NULL 
+ else CONVERT(snow_1h,DECIMAL(9,3))
+ end ,
+ snow_3h =case
+ when CONVERT(snow_3h,DECIMAL(9,3)) =0 then NULL 
+ else CONVERT(snow_3h,DECIMAL(9,3))
+ end , 
+ snow_24h = case
+ when CONVERT(snow_24h,DECIMAL(9,3)) =0 then NULL 
+ else CONVERT(snow_24h,DECIMAL(9,3))
+ end , 
+snow_today = case
+ when CONVERT(snow_today,DECIMAL(9,3)) =0 then NULL 
+ else CONVERT(snow_today,DECIMAL(9,3))
+ end ,	
+ rain_1h = case
+ when CONVERT(rain_1h,DECIMAL(9,3)) =0 then NULL 
+ else CONVERT(rain_1h,DECIMAL(9,3))
+ end ,
+rain_3h = case
+ when CONVERT(rain_3h,DECIMAL(9,3)) =0 then NULL 
+ else CONVERT(rain_3h,DECIMAL(9,3))
+ end ,
+ rain_24h = case
+ when CONVERT(rain_24h,DECIMAL(9,3)) =0 then NULL 
+ else CONVERT(rain_24h,DECIMAL(9,3))
+ end ,
+rain_today	= case
+ when CONVERT(rain_today,DECIMAL(9,3)) =0 then NULL 
+ else CONVERT(rain_today,DECIMAL(9,3))
+ end ,
+seal_level = case
+ when CONVERT(seal_level,DECIMAL(9,3)) =0 then NULL 
+ else CONVERT(seal_level,DECIMAL(9,3))
+ end ,
+ grnd_level = case
+ when CONVERT(grnd_level,DECIMAL(9,3)) =0 then NULL 
+ else CONVERT(grnd_level,DECIMAL(9,3))
+ end 
